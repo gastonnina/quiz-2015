b6fdae701aee479c65d931cdf4fa6a8085637e7f
Gaston Nina <gastonnina@gmail.com>
2015-07-18 10:52:37 -0400

    Lista de Preguntas

--- models/models.js
+++ models/models.js
@@ -32,14 +32,16 @@ var Quiz = sequelize.import(path.join(__dirname,'quiz'));
 exports.Quiz = Quiz; // exportar definición de tabla Quiz
 
 // sequelize.sync() crea e inicializa tabla de preguntas en DB
-sequelize.sync().success(function() {
+sequelize.sync().then(function() {
   // success(..) ejecuta el manejador una vez creada la tabla
-  Quiz.count().success(function (count){
+  // then(..) ejecuta el manejador una vez creada la tabla
+  Quiz.count().then(function (count){ // la tabla se inicializa solo si está vacía
     if(count === 0) {   // la tabla se inicializa solo si está vacía
-      Quiz.create({ pregunta: 'Capital de Italia',
-      	            respuesta: 'Roma'
-      	         })
-      .success(function(){console.log('Base de datos inicializada')});
-    };
+      Quiz.bulkCreate( 
+        [ {pregunta: 'Capital de Italia',   respuesta: 'Roma'},
+          {pregunta: 'Capital de Portugal', respuesta: 'Lisboa'}
+        ]
+      ).then(function(){console.log('Base de datos inicializada')});
+    }
   });
 });
