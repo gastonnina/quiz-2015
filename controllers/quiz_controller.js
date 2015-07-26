8d02e0b9805df0b46a331fb4e89cdb083ca60f88
Gaston Nina <gastonnina@gmail.com>
2015-07-26 09:51:16 -0400

    Editar pregunta

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -48,7 +48,7 @@ exports.index = function(req, res) {
 // GET /quizes/:id
 exports.show = function(req, res) {
   res.render('quizes/show', { quiz: req.quiz, errors:[]});
-};
+};// req.quiz: instancia de quiz cargada con autoload
 
 
 // GET /quizes/:id/answer
@@ -63,7 +63,7 @@ exports.answer = function (req, res) {
 // GET /quizes/new
 exports.new = function(req, res) {
   var quiz = models.Quiz.build(
-    {pregunta: "Pregunta", respuesta: "Respuesta"}
+    {pregunta: "", respuesta: ""}
   );
 
   res.render('quizes/new', {quiz: quiz, errors:[]});
@@ -88,3 +88,31 @@ exports.create = function(req, res) {
     }
   );
 }; 
+
+
+// GET /quizes/:id/edit
+exports.edit = function(req, res) {
+  var quiz = req.quiz;  // req.quiz: autoload de instancia de quiz
+
+  res.render('quizes/edit', {quiz: quiz, errors: []});
+};
+
+// PUT /quizes/:id
+exports.update = function(req, res) {
+  req.quiz.pregunta  = req.body.quiz.pregunta;
+  req.quiz.respuesta = req.body.quiz.respuesta;
+
+  req.quiz
+  .validate()
+  .then(
+    function(err){
+      if (err) {
+        res.render('quizes/edit', {quiz: req.quiz, errors: err.errors});
+      } else {
+        req.quiz     // save: guarda campos pregunta y respuesta en DB
+        .save( {fields: ["pregunta", "respuesta"]})
+        .then( function(){ res.redirect('/quizes');});
+      }     // Redirección HTTP a lista de preguntas (URL relativo)
+    }
+  );
+};
